ff46139de05f38b45a2715169c349007c22e0dd3
AnaRPires <93409353+AnaRPires@users.noreply.github.com>
2022-01-07 17:40:20 +0000

    Add files via upload

---
+++ About Me - favourites.py
@@ -0,0 +1,8 @@
+#AnaPires
+#HLT 1)
+#Favourite Meal
+Starter = input ("What is your favourite starter?")
+Main = input ("What is your favourite main course?")
+Dessert = input ("What is your favourite dessert?")
+Drink = input ("What is your favourite drink?")
+print ("Your favourite meal is", Starter, "with", Main, "a delicious", Dessert, "with a glass of", Drink)
