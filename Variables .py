f896a7d27b52dd666449e2a68e11c22483311d1b
AnaRPires <93409353+AnaRPires@users.noreply.github.com>
2022-01-07 17:36:23 +0000

    Add files via upload

---
+++ Variables .py
@@ -0,0 +1,13 @@
+#AnaPires
+#Variables
+#Price Stock
+price = 10
+stock = 5
+print( price )
+print (stock)
+print( price + stock )
+total = price * stock
+print( total)
+price = stock
+print( price )
+print( PRICE )
