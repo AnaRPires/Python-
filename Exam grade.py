6f2db0ca39a98f42a0dd96e5ce2ef7e12447a9d2
AnaRPires <93409353+AnaRPires@users.noreply.github.com>
2022-01-07 17:42:20 +0000

    Add files via upload

---
+++ Exam grade.py
@@ -0,0 +1,17 @@
+#AnaPires
+#HLT 2)
+#Student Mark in a Exam
+grade = int(input ("What is your exam grade? "))
+if grade >= 90:
+  print("grade A*")
+elif grade >= 80:
+  print("grade A")  
+elif grade >= 70:
+  print("grade B")
+elif grade >= 60:
+  print("grade C") 
+else :
+  print("have another go!")  
+
+  
+
