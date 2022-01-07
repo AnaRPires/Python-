86e168eee10cce07c0924e74206113d47be425be
AnaRPires <93409353+AnaRPires@users.noreply.github.com>
2022-01-07 16:14:59 +0000

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,13 @@
+#AnaPires TTA
+#My first code
+print ("hello world")
+print ("hey Ana")
+
+salary = 200
+print (salary)
+salary = salary + 50
+print (salary)
+salary = salary + salary
+print (salary)
+salary = salary * salary
+print (salary)
