faee3f470dfbfa87c11daf2e0808a33f22483a41
AnaRPires <93409353+AnaRPires@users.noreply.github.com>
2022-01-18 13:31:09 +0000

    Add files via upload

---
+++ Assigning a Variable.py
@@ -0,0 +1,18 @@
+#AnaPires
+#Assigning a Variable
+#Phone
+#ProductName = "iPhone"
+#ProductPrice = 450
+#print (ProductName)
+#print (ProductPrice)
+#print (ProductName, #ProductPrice)
+#print ("Your ",ProductName,"costs £",ProductPrice)
+
+phone = input("what phone do you have?")
+cost = int(input("how much is your phone worth?"))
+print("your phone is", phone, "and it will be worth", cost -100, "next year")
+
+
+
+
+
